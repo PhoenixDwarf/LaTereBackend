a845139801f035c841b11303cfb9a1c289e0108c
Edwin <95711147+PhoenixDwarf@users.noreply.github.com>
2022-04-29 20:15:18 -0500

    make it better5

--- src/database.js
+++ src/database.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'LaTereDB'
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'b23d01428accd4',
+    password: '3370f343',
+    database: 'heroku_ce53df1a5b9003a'
 })
-
 mysqlConnection.connect(function (err){
     if(err){
         console.log(err);
