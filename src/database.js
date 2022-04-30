4c132e71bb31b20a7c1e38c22ebc90a8458f03c7
Edwin <95711147+PhoenixDwarf@users.noreply.github.com>
2022-04-29 20:52:41 -0500

    make it better

--- src/database.js
+++ src/database.js
@@ -1,11 +1,28 @@
 const mysql = require('mysql');
 
-const mysqlConnection = mysql.createConnection({
+/*const mysqlConnection = mysql.createConnection({
     host: 'us-cdbr-east-05.cleardb.net',
     user: 'b23d01428accd4',
     password: '3370f343',
     database: 'heroku_ce53df1a5b9003a'
+})*/
+
+/*
+const mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'lateredb'
 })
+*/
+
+const mysqlConnection = mysql.createConnection({
+    host: process.env.host ,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+})
+
 mysqlConnection.connect(function (err){
     if(err){
         console.log(err);
