697cf7307018e596409c4985e811ad2a6177e946
Edwin <95711147+PhoenixDwarf@users.noreply.github.com>
2022-05-01 13:53:37 -0500

    make it better

--- src/database.js
+++ src/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
-/*const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createConnection({
     host: 'us-cdbr-east-05.cleardb.net',
     user: 'b23d01428accd4',
     password: '3370f343',
     database: 'heroku_ce53df1a5b9003a'
-})*/
+})
 
 /*
 const mysqlConnection = mysql.createConnection({
@@ -15,13 +15,14 @@ const mysqlConnection = mysql.createConnection({
     database: 'lateredb'
 })
 */
-
+/*
 const mysqlConnection = mysql.createConnection({
     host: process.env.host ,
     user: process.env.user,
     password: process.env.password,
     database: process.env.database
 })
+*/
 
 
 var connection;
